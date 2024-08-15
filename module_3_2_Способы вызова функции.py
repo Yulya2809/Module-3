d0367704a5938ff7ecd2739de9556e628d48e90a
Yulya2809 <93946203+Yulya2809@users.noreply.github.com>
2024-08-15 19:53:23 +0300

    Add files via upload

--- module_3_2_Способы вызова функции.py
+++ module_3_2_Способы вызова функции.py
@@ -2,10 +2,10 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
     if "@" not in recipient or "@" not in sender or not sender.endswith((".com", ".ru", ".net")) or not recipient.endswith((".com", ".ru", ".net")):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
-    if sender == recipient:
+    elif sender == recipient:
         print(f"Нельзя отправить письмо самому себе!")
 
-    if sender == "university.help@gmail.com":
+    elif sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
